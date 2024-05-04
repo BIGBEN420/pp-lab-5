5f1f03ca44bf108d732fe5c4a79bf91e4e4038ca
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-05-04 11:25:21 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,17 @@
-
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
+public class test {
+    public static void main(String[] args) {
+        try {
+            Person person = new Person("Bartek", 20);
+            System.out.println("Imię: " + person.getName());
+            System.out.println("Wiek: " + person.getAge());
+        } catch (InvalidAgeException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        Messenger messenger = new EmailMessenger();
+        messenger.sendMessage("Test");
+    }
+}
