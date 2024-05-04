41ff155be1350b9efd6c224ed20aaf73df80b607
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-05-04 11:32:19 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
-public class test {
+public class Main {
     public static void main(String[] args) {
         try {
             Person person = new Person("Bartek", 20);
