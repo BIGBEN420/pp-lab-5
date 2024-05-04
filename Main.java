44e18b44e1b05bca55e4b56562fa5555064bf882
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-05-04 12:00:30 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,18 +6,30 @@ import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        int licznik = 1;
+        Person[] people = new Person[5];
+        int b = 10;
         try {
-            Person person = new Person("Bartek", 20);
-            System.out.println("Imię: " + person.getName());
-            System.out.println("Wiek: " + person.getAge());
+            people[0] = new Person("Jacek", 39);
+            people[1] = new Person("Wojtek", 60);
+            people[2] = new Person("Artur", 39);
+            people[3] = new Person("Przemek", 45);
+            people[4] = new Person("Tomek", 50);
+                
         } catch (InvalidAgeException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        int a = 5;
-        int b = 6;
-        int suma = MathUtils.add(a,b);
+        for (Person o : people){
+            System.out.println("Informacje o osobie "+licznik);
+            System.out.println("Imie: "+o.getName());
+            System.out.println("Wiek: "+o.getAge());
+            int sum = MathUtils.add(o.getAge(), b);
+            System.out.println("Wartość sumy wieku i b to: " + sum);
+            Messenger messenger = new EmailMessenger();
+            messenger.sendMessage("Wynik sumy wieku i b to: " + sum);
+            System.out.println();
+            licznik++;
+        }
         
-        Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Test sumy: "+suma);
     }
 }
