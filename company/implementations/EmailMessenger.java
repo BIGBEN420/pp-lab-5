22853c29ce64221b4f1668c8fb600c51e2c9080c
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-05-04 11:24:35 +0200

    Update EmailMessenger.java

--- company/implementations/EmailMessenger.java
+++ company/implementations/EmailMessenger.java
@@ -1,6 +1,7 @@
 package company.implementations;
 import company.interfaces.Messenger;
 public class EmailMessenger implements Messenger {
+    @Override
     public void sendMessage(String message) {
         System.out.println("Wiadomość: " + message);
     }
