b4981e320694edbd1f39656eaf372cdf5cdf4c01
Bartłomiej Krawczyk <65669877+BIGBEN420@users.noreply.github.com>
2024-05-04 11:32:00 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
-public class test {
+import company.utils.MathUtils;
+
+public class Main {
     public static void main(String[] args) {
         try {
             Person person = new Person("Bartek", 20);
@@ -11,7 +13,11 @@ public class test {
         } catch (InvalidAgeException e) {
             System.out.println("Error: " + e.getMessage());
         }
+        int a = 5;
+        int b = 6;
+        int suma = MathUtils.add(a,b);
+        
         Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Test");
+        messenger.sendMessage("Test sumy: "+suma);
     }
 }
